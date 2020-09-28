3319031f01795c8f41d4a7506eec17c520444611
Braulio Diez Botella <braulio.diez@gmail.com>
2020-09-28 18:40:29 +0200

    take latest ready

---
+++ 01-sagas/02-take-latest/src/main.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { NumberViewerContainer, NumberSetterContainer } from "./components";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <>
+      <NumberSetterContainer />
+      <NumberViewerContainer />
+    </>
+  </Provider>,
+  document.getElementById("root")
+);
